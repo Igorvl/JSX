26a2f0f464c18a91b3cb23051d5f0deb2f81dcb4
IgorVL <mail@igorvl.ru>
2020-04-29 06:35:14 +0300

    Samura_rev2 16 props

--- src/components/Main/Profile/MyPosts/MyPosts.js
+++ src/components/Main/Profile/MyPosts/MyPosts.js
@@ -9,18 +9,11 @@ export default () => {
 			<div>
 				<textarea name="#" id="" cols="60" rows="5">111</textarea>
 			</div>
-			<div>
-				<button>Like</button>
-				<button>Dislike</button>
-			</div>
 			<div className={s.posts}>
-				<Post/>
-				<Post/>
-				<Post/>
-				<Post/>
+				<Post msg='Message 1 Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.' like='5' dislike='3'/>
+				<Post msg='Message 2 Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.' like='3' dislike='2'/>
+				<Post msg='Message 3 Lorem ipsum dolor sit amet.Lore' like='2' dislike='5'/>
 			</div>
-
-
 		</div>
 	);
 };
--- src/components/Main/Profile/MyPosts/Post/Post.js
+++ src/components/Main/Profile/MyPosts/Post/Post.js
@@ -2,11 +2,15 @@ import React, {Component} from 'react';
 import s from '../../../../../css/Post.module.css';
 import icon from '../../../../../Images/logo.svg'
 
-export default () => {
+export default (props) => {
 	return (
 		<div className={s.mainBlock}>
 			<img src={icon} alt=""/>
-			<p>Post 1</p>
+			<p>{props.msg}</p>
+			<div>
+				<button>Like</button> <span>{props.like}</span>
+				<button>Dislike</button> <span>{props.dislike}</span>
+			</div>
 		</div>
 	);
 };
