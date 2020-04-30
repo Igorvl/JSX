29191994889a0cecf90c301784c7cede6ef83998
IgorVL <mail@igorvl.ru>
2020-04-30 04:04:34 +0300

    Samura_rev2 20 NavLink

--- src/components/Main/Navbar.js
+++ src/components/Main/Navbar.js
@@ -1,58 +1,59 @@
-import React, {Component} from 'react';
+import React from 'react';
 import s from '../../css/Navbar.module.css';
 import logoNav from "../../Images/logo.svg"
+import {NavLink} from "react-router-dom";
 
 export default () => {
 	return (
 				<nav className={s.leftNav}>
 					<ul className={s.nav_ul}>
 						<li>
-							<a href="/profile" className={s.nav_link}>
+							<NavLink to="/profile" className={s.nav_link} activeClassName={s.nav_linkActive}>
 								<img src={logoNav} className={s.logo_nav} alt=""/>
 								<span className={s.nav_txt}>
 										Profile
 									</span>
-							</a>
+							</NavLink>
 						</li>
 						<li>
-							<a href="/messages" className={s.nav_link}>
+							<NavLink to="/messages" className={s.nav_link} activeClassName={s.nav_linkActive}>
 								<img src={logoNav} className={s.logo_nav} alt=""/>
 								<span className={s.nav_txt}>
 										Messages
 									</span>
-							</a>
+							</NavLink>
 						</li>
 						<li>
-							<a href="/news" className={s.nav_link}>
+							<NavLink to="/news" className={s.nav_link} activeClassName={s.nav_linkActive}>
 								<img src={logoNav} className={s.logo_nav} alt=""/>
 								<span className={s.nav_txt}>
 										News
 									</span>
-							</a>
+							</NavLink>
 						</li>
 						<li>
-							<a href="/music" className={s.nav_link}>
+							<NavLink to="/music" className={s.nav_link} activeClassName={s.nav_linkActive}>
 								<img src={logoNav} className={s.logo_nav} alt=""/>
 								<span className={s.nav_txt}>
 										Music
 									</span>
-							</a>
+							</NavLink>
 						</li>
 						<li>
-							<a href="/games" className={s.nav_link}>
+							<NavLink to="/games" className={s.nav_link} activeClassName={s.nav_linkActive}>
 								<img src={logoNav} className={s.logo_nav} alt=""/>
 								<span className={s.nav_txt}>
 										Games
 									</span>
-							</a>
+							</NavLink>
 						</li>
 						<li>
-							<a href="/settings" className={s.nav_link}>
+							<NavLink to="/settings" className={s.nav_link} activeClassName={s.nav_linkActive}>
 								<img src={logoNav} className={s.logo_nav} alt=""/>
 								<span className={s.nav_txt}>
 										Settings
 									</span>
-							</a>
+							</NavLink>
 						</li>
 					</ul>
 				</nav>
